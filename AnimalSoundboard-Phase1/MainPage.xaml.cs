2be8ea74fb9f344914cb1368faf868cfeceb3dd8
Zachary Teutsch <zteutsch@microsoft.com>
2021-09-30 16:58:17 -0700

    Added comments to Phase 1

--- AnimalSoundboard-Phase1/MainPage.xaml.cs
+++ AnimalSoundboard-Phase1/MainPage.xaml.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿// This is the C# code file asssociated with your XAML file.
+// Simple logic can be added to this file and bound to your XAML file to add functionality to your app.
+// Together, these two files are the core of any page you create for a simple UWP app.
+
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.Storage;
-using System.Threading.Tasks;
-using Windows.ApplicationModel;
-using Windows.Storage.Streams;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace AnimalSoundboard_Phase1
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
+    // Here is the class that represents your MainPage.
+    // Like all Pages, it inherits from the Page class so you can focus on whats unique to your page, rather than UWP pages in general.
+
     public sealed partial class MainPage : Page
-    {
+    {   
+
+        // Constructor for your page.
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        // Here is the function that your Cow Button binds to.
+        // It reads a .wav file into a stream and sets that stream as a MediaElement source to be played.
         private async void Cow_Button_Click(object sender, RoutedEventArgs e)
         {
             MediaElement mysong = new MediaElement();
@@ -41,6 +35,7 @@ namespace AnimalSoundboard_Phase1
             mysong.Play();
         }
 
+        // Same functionality as above but for Chicken Button
         private async void Chicken_Button_Click(object sender, RoutedEventArgs e)
         {
             MediaElement mysong = new MediaElement();
